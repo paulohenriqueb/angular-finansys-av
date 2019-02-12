c0f108601f8a5c695790806b158bc9138205709a
Paulo Henrique Barbosa da Silva <paulohenriqueb.silva@gmail.com>
2019-02-12 18:29:12 -0200

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
-        const categories = [
+        const categories : Category[] = [
             { id: 1, name: "Moradia", description: "Pagamentos de contas da casa"},
             { id: 2, name: "Saúde", description: "Plano de saúde e Remédios"},
             { id: 3, name: "Lazer", description:"Cinema, parques, praia, etc"},
-            { id: 4, nmae: "Salário", description: "Recebimento de Salário"},
-            { id: 5, name: "Freelas", description: "Trabalhos com freelancer"}
+            { id: 4, name: "Salário", description: "Recebimento de Salário"},
+            { id: 5, name: "Freelas", description: "Trabalhos com freelancer"},
         ];
     return { categories }
     }
